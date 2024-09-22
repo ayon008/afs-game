1d130215606c8dd2c24912069e839fc3b0ba12b7
ayon008 <shariar.ayon128@gmail.com>
2024-09-22 19:15:25 +0600

    f-award

--- src/app/(Main)/award/page.js
+++ src/app/(Main)/award/page.js
@@ -15,10 +15,10 @@ const page = async () => {
                     <h1 className={`${morgana.className} text-center 2xl:text-9xl xl:text-7xl text-5xl text-white uppercase`}>Award</h1>
                 </div>
             </div>
-            <div className='bg-white 2xl:p-20 xl:p-20 p-10 rounded-t-[50px]'>
+            <div className='bg-white 2xl:p-20 xl:p-20 p-6 rounded-t-[50px]'>
                 <h2 className={`${morgana.className} uppercase text-center 2xl:text-7xl xl:text-5xl text-2xl`}>découvrez les partenaires de <br />l’événement</h2>
 
-                <div className='grid 2xl:grid-cols-2 xl:grid-cols-2 grid-cols-1 2xl:mt-20 xl:mt-16 mt-10 2xl:gap-10 xl:gap-8 gap-y-4 2xl:justify-center xl:justify-center justify-normal'>
+                <div className='grid 2xl:grid-cols-2 xl:grid-cols-2 grid-cols-1 2xl:mt-20 xl:mt-16 mt-10 2xl:gap-10 xl:gap-8 gap-y-6 2xl:justify-center xl:justify-center justify-normal'>
                     {
                         sponsors.map((sponsor, i) => {
                             return (
@@ -41,51 +41,6 @@ const page = async () => {
                 <p className='text-[#0000007f] 2xl:text-2xl xl:text-lg text-sm font-semibold text-center 2xl:my-12 xl:my-6 my-4'>Temps passé à l’eau en wingfoil, downwind, windfoil, dockstart, surf foil</p>
                 <Award />
             </div>
-            {/* <div className='2xl:m-[10px] m-1 relative'>
-                <Image
-                    src={image}
-                    alt='banner-image'
-                    placeholder='blur'
-                    className='w-full max-h-auto min-h-[20vh]'
-                />
-                <h2 className={`${antiHero.className} text-white 2xl:text-7xl xl:text-5xl text-lg font-bold absolute 2xl:left-24 xl:left-24 2xl:bottom-10 xl:bottom-10 left-10 bottom-5`}>What you can win!</h2>
-            </div>
-            <Sponsor />
-            <p className="text-center font-semibold uppercase text-[#111111BF] xl:text-xs 2xl:text-lg text-[6px]">
-                classement général — La principale récompense
-            </p>
-            <h2 className={`${antiHero.className} text-center font-bold xl:text-4xl text-lg text-black 2xl:text-5xl xl:mt-2 2xl:mt-5`}>
-                <span className="text-blue-500">Watermen Crown</span> Wingfoil,
-                <br />
-                Windfoil, Dockstart, Surffoil
-            </h2>
-            <div className='2xl:px-36 xl:px-20 2xl:mt-10 xl:mt-8 mt-4 px-6'>
-                <div className='2xl:h-[320px] xl:h-[300px] relative'>
-                    <Image
-                        src={priceImage}
-                        alt='watermen-crown'
-                        placeholder='blur'
-                        className='w-full h-full 2xl:rounded-[10px] xl:rounded-[10px] rounded object-cover'
-                    />
-                    <div class="absolute inset-0 bg-gradient-to-tr from-transparent to-black opacity-40"></div>
-                    <div className='inset-0 absolute z-20 p-4 flex flex-col h-full'>
-                        <h2 className={`${antiHero.className} text-[#FAE500] font-normal 2xl:text-7xl xl:text-4xl`}>#1.</h2>
-                        <p className='2xl:text-6xl xl:text-4xl font-semibold text-white mt-auto'>Exclusive Garmin Enduro 2 Set (smartwatch + navigation equipment)</p>
-                    </div>
-                </div>
-                <div className='2xl:mt-40 xl:mt-28 mt-12'>
-                    <h2 className={`${antiHero.className} text-center font-bold xl:text-4xl 2xl:mb-10 xl:mb-6 mb-2 2xl:text-5xl xl:mt-2 2xl:mt-5 text-blue-500 text-lg`}>
-                        Tous les prix dans les <br /> autres nominations
-                    </h2>
-                    <div className='custom-grid border-t-2 border-black'>
-                        <h5 className='2xl:text-lg xl:text-sm text-xs font-semibold 2xl:mt-[18px] xl:mt-3 mt-2 2xl:text-left xl:text-left text-center uppercase '>Temps passé à l’eau sur la <br /> durée de l’événement</h5>
-                        <div>
-                            <TopAchiver />
-                            <TopAchiver />
-                        </div>
-                    </div>
-                </div>
-            </div> */}
             <Join />
         </div >
     );
--- src/ui/Award.jsx
+++ src/ui/Award.jsx
@@ -1,4 +1,6 @@
 'use client'
+import GetAwardsByCategory from '@/lib/getAwardByCategory';
+import GetAwards from '@/lib/getAwards';
 import React, { useState } from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
@@ -7,59 +9,92 @@ const Award = () => {
     const [tabIndex, setTabIndex] = useState(0);
     return (
         <div className='overflow-x-auto mt-4'>
-            <Tabs>
-                <TabList className={'flex items-center justify-center gap-10 cursor-pointer'}>
+            <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList className={'flex items-center justify-center 2xl:gap-10 xl:gap-10 gap-3 cursor-pointer'}>
                     {
                         categories.map((category, i) => {
                             return (
-                                <Tab key={i} className={`${tabIndex === i && 'text-blue-500 pb-1 border-b-2 border-blue-500'} 2xl:text-lg xl:text-sm font-semibold uppercase pb-1`}>{category}</Tab>
+                                <Tab key={i} className={`${tabIndex === i && 'text-blue-500 pb-1 border-b-2 border-blue-500'} 2xl:text-lg xl:text-sm text-[8px] font-semibold uppercase pb-1`}>{category}</Tab>
                             )
                         })
                     }
                 </TabList>
-                <TabPanel>
-                    <div className='2xl:mt-14 xl:mt-8 mt-6 grid grid-cols-3 items-end 2xl:gap-6 xl:gap-5 gap-3'>
-                        <div className='2xl:p-10 xl:p-6 p-4 award 2xl:h-[490px] xl:h-[350px] h-[270px]'>
-                            <h1 className='2xl:text-8xl xl:text-6xl text-4xl font-bold text-white'>
-                                1<sup className='2xl:text-3xl xl:text-xl text-lg font-medium align-super'>ER</sup>
-                            </h1>
-                            <ul className='text-[#FFFFFF80] list-disc pl-5 2xl:mt-14 xl:mt-10 mt-6'>
-                                <li>AFS Whitebird 5’8 Artness Pro A3D</li>
-                                <li>Cale AP3D</li>
-                                <li>Abonnement annuel Foiling Mag</li>
-                            </ul>
-                            <div>
+                {
+                    categories.map((category, i) => {
+                        const { isLoading, isError, error, awardsCategory, refetch } = GetAwardsByCategory(category);
+                        console.log(awardsCategory);
 
-                            </div>
-                        </div>
-                        <div className='2xl:p-10 xl:p-6 p-4 award 2xl:h-[394px] xl:h-[290px] h-[190px]'>
-                            <h1 className='2xl:text-8xl xl:text-6xl text-4xl font-bold text-white'>
-                                1<sup className='2xl:text-3xl xl:text-xl text-lg font-medium align-super'>ER</sup>
-                            </h1>
-                            <ul className='text-[#FFFFFF80] list-disc pl-5 2xl:mt-14 xl:mt-10 mt-6'>
-                                <li>AFS Whitebird 5’8 Artness Pro A3D</li>
-                                <li>Cale AP3D</li>
-                                <li>Abonnement annuel Foiling Mag</li>
-                            </ul>
-                            <div>
 
-                            </div>
-                        </div>
-                        <div className='2xl:p-10 xl:p-6 p-4 award 2xl:h-[330px] xl:h-[230px] h-[150px]'>
-                            <h1 className='2xl:text-8xl xl:text-6xl text-4xl font-bold text-white'>
-                                1<sup className='2xl:text-3xl xl:text-xl text-lg font-medium align-super'>ER</sup>
-                            </h1>
-                            <ul className='text-[#FFFFFF80] list-disc pl-5 2xl:mt-14 xl:mt-10 mt-6'>
-                                <li>AFS Whitebird 5’8 Artness Pro A3D</li>
-                                <li>Cale AP3D</li>
-                                <li>Abonnement annuel Foiling Mag</li>
-                            </ul>
-                            <div>
+                        return (
+                            <TabPanel>
+                                <div className='2xl:mt-14 xl:mt-8 mt-6 grid grid-cols-3 items-end 2xl:gap-6 xl:gap-5 gap-3'>
+                                    {
+                                        awardsCategory?.map((a, i) => {
+                                            if (a.position === '1st') {
+                                                return (
+                                                    <div className='2xl:p-10 xl:p-6 p-4 award 2xl:h-[490px] xl:h-[410px] h-[340px] flex flex-col'>
+                                                        <h1 className='2xl:text-8xl xl:text-6xl text-4xl font-bold text-white'>
+                                                            1<sup className='2xl:text-3xl xl:text-xl text-lg font-medium align-super'>ER</sup>
+                                                        </h1>
+                                                        <ul className='text-[#FFFFFF80] list-disc 2xl:pl-5 xl:pl-5 pl-2 2xl:mt-14 xl:mt-10 mt-6'>
+                                                            <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize1}</li>
+                                                            {a.prize2 && <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize2}</li>}
+                                                            {a.prize3 && <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize3}</li>}
+                                                        </ul>
+                                                        <div className='mt-auto grid grid-cols-3 gap-2 w-full overflow-hidden items-end'>
+                                                            <img src={a.sponsors1} className='w-full max-h-[65px]' alt='' />
+                                                            <img src={a.sponsors2} className='w-full max-h-[65px]' alt='' />
+                                                            <img src={a.sponsors3} className='w-full max-h-[65px]' alt='' />
+                                                        </div>
+                                                    </div>
+                                                )
+                                            }
+                                            if (a.position === '2nd') {
+                                                return (
+                                                    <div className='2xl:p-10 xl:p-6 p-4 award 2xl:h-[430px] xl:h-[340px] h-[280px] flex flex-col'>
+                                                        <h1 className='2xl:text-8xl xl:text-6xl text-4xl font-bold text-white'>
+                                                            2<sup className='2xl:text-3xl xl:text-xl text-lg font-medium align-super'>EME</sup>
+                                                        </h1>
+                                                        <ul className='text-[#FFFFFF80] list-disc 2xl:pl-5 xl:pl-5 pl-2 2xl:mt-14 xl:mt-10 mt-6'>
+                                                            <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize1}</li>
+                                                            {a.prize2 && <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize2}</li>}
+                                                            {a.prize3 && <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize3}</li>}
+                                                        </ul>
+                                                        <div className='mt-auto grid grid-cols-3 gap-2 w-full overflow-hidden items-end'>
+                                                            <img src={a.sponsors1} className='w-full max-h-[65px]' alt='' />
+                                                            <img src={a.sponsors2} className='w-full max-h-[65px]' alt='' />
+                                                            <img src={a.sponsors3} className='w-full max-h-[65px]' alt='' />
+                                                        </div>
+                                                    </div>
+                                                )
+                                            }
+                                            if (a.position === '3rd') {
+                                                return (
+                                                    <div className='2xl:p-10 xl:p-6 p-4 award 2xl:h-[380px] xl:h-[290px] h-[200px] flex flex-col'>
+                                                        <h1 className='2xl:text-8xl xl:text-6xl text-4xl font-bold text-white'>
+                                                            3<sup className='2xl:text-3xl xl:text-xl text-lg font-medium align-super'>EME</sup>
+                                                        </h1>
+                                                        <ul className='text-[#FFFFFF80] list-disc 2xl:pl-5 xl:pl-5 pl-2 2xl:mt-14 xl:mt-10 mt-6'>
+                                                            <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize1}</li>
+                                                            {a.prize2 && <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize2}</li>}
+                                                            {a.prize3 && <li className='2xl:text-2xl xl:text-lg text-xs'>{a.prize3}</li>}
+                                                        </ul>
+                                                        <div className='mt-auto grid grid-cols-3 gap-2 w-full overflow-hidden items-end'>
+                                                            <img src={a.sponsors1} className='w-full max-h-[65px]' alt='' />
+                                                            <img src={a.sponsors2} className='w-full max-h-[65px]' alt='' />
+                                                            <img src={a.sponsors3} className='w-full max-h-[65px]' alt='' />
+                                                        </div>
+                                                    </div>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </div>
+                            </TabPanel>
+                        )
+                    })
+                }
 
-                            </div>
-                        </div>
-                    </div>
-                </TabPanel>
             </Tabs>
         </div>
     );
