eb84e84135122fedb3bf35df41c730bda3aa98a8
Al-Shariar Auon <118975535+ayon008@users.noreply.github.com>
2024-11-19 17:21:43 +0600

    leader page.js

--- src/app/(Main)/leaderboard/page.js
+++ src/app/(Main)/leaderboard/page.js
@@ -22,30 +22,7 @@ const page = async () => {
                 <h2 className={`${morgana.className} uppercase m-auto text-white 2xl:text-[120px] xl:text-7xl text-5xl`}>leaderboard</h2>
             </div>
             <div className='2xl:mt-20 xl:mt-14 mt-8 bg-white 2xl:p-20 xl:p-20 py-12 rounded-tr-[50px] rounded-tl-[50px]'>
-                
-                   
-                     
-                        <div>
-                            <h3 className={`${morgana.className} uppercase 2xl:text-6xl xl:text-4xl text-2xl text-center`}>*The leaderboard has disappeared!*</h3>
-                            <p className='text-center mt-10 text-lg 2xl:px-0 xl:px-0 px-10'>
-                                
-
-
-The AFS Games leaderboard has disappeared! 😱
-
-Don't worry, this was planned by our team. As the challenge nears its end, we’re working on the closing ceremony and prize-giving event. The leaderboard will remain offline until the day after the ceremony!
-
-Nothing is decided yet! Keep uploading your sessions until the very end!
-
-This little surprise is our way of keeping you on your toes and encouraging you to join the award ceremony.
-
-See you soon for the big reveal!
-
-
-                
-                            </p>
-                        </div>
-                
+                <SelectTab pointTable={pointTable} />
             </div>
             <Join />
         </div>
